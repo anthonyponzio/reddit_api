d376d22fdb5a9e4bd54af8a09126b690709172f2
anthony ponzio <anthonyponzio1992@gmail.com>
2020-08-12 02:37:05 -0400

    trim space in router file

--- src/kittens/router.js
+++ src/kittens/router.js
@@ -4,7 +4,7 @@ const Kitten = require('./model')
 const router = new express.Router()
 
 router.post('/kittens', async (req, res) => {
-	const kitten = new  Kitten(req.body)
+	const kitten = new Kitten(req.body)
 
 	try {
 		await kitten.save()
@@ -17,9 +17,7 @@ router.post('/kittens', async (req, res) => {
 router.get('/kittens/:id', async (req, res) => {
 	try {
 		const kitten = await Kitten.findOne({ _id: req.params.id })
-
 		if (!kitten) { return res.status(404).send() }
-
 		res.send(kitten)
 	} catch (e) {
 		res.status(500).send()
@@ -30,7 +28,6 @@ router.patch('/kittens/:id', async (req, res) => {
 	try {		
 		const kitten = await Kitten.findOne({ _id: req.params.id })
 		if (!kitten) { return res.status(404).send() }
-
 		kitten.name = req.body.name
 		await kitten.save()
 		res.send(kitten)
@@ -42,7 +39,6 @@ router.patch('/kittens/:id', async (req, res) => {
 router.delete('/kittens/:id', async (req, res) => {
 	try {
 		const kitten = await Kitten.findOneAndDelete({ _id: req.params.id	})
-		
 		if (!kitten) { return res.status(404).send() }
 		res.send(kitten)
 	} catch (e) {
@@ -52,4 +48,3 @@ router.delete('/kittens/:id', async (req, res) => {
 })
 
 module.exports = router
-

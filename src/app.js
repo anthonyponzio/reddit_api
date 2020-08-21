aef0f06a6fcc318d07249c3f9dcc8d3e096f9779
anthony ponzio <anthonyponzio1992@gmail.com>
2020-08-21 02:13:17 -0400

    Remove kitten test model and router

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 require('./db/mongoose')
 const express = require('express')
-const kittensRouter = require('./kittens/router')
 const usersRouter = require('./users/router')
 
 const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
-app.use(kittensRouter)
 app.use(usersRouter)
 
 
